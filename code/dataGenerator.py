efda0c904d94771f6ecb6d1f968fc36964a7c0ea
hbchen <k160438@sjtu.edu.cn>
2019-05-10 23:25:19 +0800

    data preprocessing finish

--- code/dataGenerator.py
+++ code/dataGenerator.py
@@ -7,14 +7,17 @@ rootPath = os.path.dirname(os.path.dirname(__file__))
 sys.path.append(rootPath)
 np.random.seed(101)
 
+# the pixels to padding on each border
 padding = 500
 
-def PostiveImages():
+
+def PostiveSamples():
     X_train = []
     X_test = []
-
+    print("Generating positive samples...")
     for i in range(1, 11):
-        filename = 'data/FDDB-folds/FDDB-fold-{}-ellipseList.txt'.format(str(i).zfill(2))
+        filename = 'data/FDDB-folds/FDDB-fold-{}-ellipseList.txt'.format(
+            str(i).zfill(2))
         images_l = open(filename)
         imgName = images_l.readline().strip()
         while imgName != '':
@@ -34,13 +37,13 @@ def PostiveImages():
                 # print(major_radius, minor_radius, angle, center_x, center_y)
                 major_radius, minor_radius, angle, center_x, center_y = float(major_radius), float(
                     minor_radius), float(angle), float(center_x)+padding, float(center_y)+padding
-                width = minor_radius*np.abs(np.sin(angle))*8/3
-                height = major_radius*np.abs(np.sin(angle))*8/3
+                width = minor_radius*8/3
+                height = major_radius*8/3
                 # if center_y<height/2 or center_x < width/2 or center_y+height/2 > img.shape[0] or center_x + width/2 > img.shape[1]:
                 #     print("Outside of image!")
                 #     print(major_radius, minor_radius)
                 face = img[int(center_y-height/2):int(center_y+height/2),
-                        int(center_x - width/2):int(center_x + width/2)]
+                           int(center_x - width/2):int(center_x + width/2)]
                 try:
                     face = cv2.resize(face, (96, 96))
                 except:
@@ -59,11 +62,98 @@ def PostiveImages():
             # cv2.imwrite("data/visual/positive_{}.jpg".format(imgName.replace('/', '_')), img)
             imgName = images_l.readline().strip()
         print("{} loading finish!".format(filename))
-    
+
     return np.array(X_train), np.array(X_test)
 
 
+def NegativeSamples():
+    X_train = []
+    X_test = []
+    print("Generating negative samples...")
+    for i in range(1, 11):
+        if i > 4 and i < 10:
+            continue
+        filename = 'data/FDDB-folds/FDDB-fold-{}-ellipseList.txt'.format(
+            str(i).zfill(2))
+        images_l = open(filename)
+        imgName = images_l.readline().strip()
+        while imgName != '':
+            img = cv2.imread("data/{}.jpg".format(imgName))
+            img = cv2.copyMakeBorder(
+                img, padding, padding, padding, padding, borderType=cv2.BORDER_REPLICATE)
+            try:
+                numOfFaces = int(images_l.readline().strip())
+            except:
+                print('a')
+                print(type(imgName))
+                # print(imgName, images_l.readlines())
+                exit()
+
+            for j in range(numOfFaces):
+                major_radius, minor_radius, angle, center_x, center_y, _ = images_l.readline().split()
+                # print(major_radius, minor_radius, angle, center_x, center_y)
+                major_radius, minor_radius, angle, center_x, center_y = float(major_radius), float(
+                    minor_radius), float(angle), float(center_x)+padding, float(center_y)+padding
+                width = minor_radius
+                height = major_radius
+                delta_a = height*2/3
+                delta_b = width*2/3
+                for a in range(-1, 2):
+                    for b in range(-1, 2):
+                        if a == 0 and b == 0:
+                            continue
+                        n_face = img[int(center_y-height+a*delta_a):int(center_y+height+a*delta_a),
+                                     int(center_x - width+b*delta_b):int(center_x + width+b*delta_b)]
+                        try:
+                            n_face = cv2.resize(n_face, (96, 96))
+                        except:
+                            continue
+                        # cv2.rectangle(img, (int(center_x - width+b*delta_b), int(center_y-height+a*delta_a)),
+                        #               (int(center_x+width+b*delta_b), int(center_y+height+a*delta_a)), (0, 0, 0))
+                        if i < 5:
+                            X_train.append(n_face)
+                        else:
+                            X_test.append(n_face)
+                        # print(n_face.shape)
+                        # cv2.imshow('n_face_{}'.format(j), n_face)
+
+            # cv2.imshow("visualized_positive_img", img)
+            # cv2.waitKey(0)
+            # cv2.imwrite("data/visual/negative_{}.jpg".format(imgName.replace('/', '_')), img)
+            imgName = images_l.readline().strip()
+        print("{} loading finish!".format(filename))
+
+    return np.array(X_train), np.array(X_test)
+
+
+def GetDataset():
+    X_train_p, X_test_p = PostiveSamples()
+    X_train_n, X_test_n = NegativeSamples()
+    print("Generate positive samples:\nTraining: {}\nTest: {}".format(
+        X_train_p.shape, X_test_p.shape))
+    print("Generate negative samples:\nTraining: {}\nTest: {}".format(
+        X_train_n.shape, X_test_n.shape))
+    y_train_p = np.ones(len(X_train_p))
+    y_test_p = np.ones(len(X_test_p))
+    y_train_n = - np.ones(len(X_train_n))
+    y_test_n = -np.ones(len(X_test_n))
+    X_train = np.concatenate((X_train_p, X_train_n), axis=0)
+    X_test = np.concatenate((X_test_p, X_test_n), axis=0)
+    y_train = np.concatenate((y_train_p, y_train_n), axis=0)
+    y_test = np.concatenate((y_test_p, y_test_n), axis=0)
+    indices_train = np.arange(len(X_train))
+    indices_test = np.arange(len(X_test))
+    np.random.shuffle(indices_train)
+    np.random.shuffle(indices_test)
+    return X_train[indices_train], X_test[indices_test], y_train[indices_train], y_test[indices_test]
+
 if __name__ == "__main__":
-    X_train, X_test = PostiveImages()
+    X_train, X_test, y_train, y_test = GetDataset()
     print(X_train.shape)
     print(X_test.shape)
+    print(y_train.shape)
+    print(y_test.shape)
+    np.save('data/X_train', X_train)
+    np.save('data/X_test', X_test)
+    np.save('data/y_train', y_train)
+    np.save('data/y_test', y_test)
---
+++ code/hog.py
@@ -0,0 +1,47 @@
+import os
+import sys
+from skimage.feature import hog
+import numpy as np
+import cv2
+
+rootpath = os.path.dirname(os.path.dirname(__file__))
+sys.path.append(rootpath)
+
+
+if __name__=="__main__":
+    X_train = np.load('data/X_train.npy')
+    X_test = np.load('data/X_test.npy')
+    X_train_hog = []
+    X_test_hog = []
+    print("Extract HOG features of training samples...")
+    for i in range(X_train.shape[0]):
+        if (i+1) % 1000==0:
+            print("{}/{}".format(i+1, X_train.shape[0]))
+        feature, hog_img = hog(X_train[i], 9, (16, 16), (2, 2), visualise=True, feature_vector=True)
+        X_train_hog.append(feature)
+    X_train_hog = np.array(X_train_hog)
+    print("Finish extracting training samples!")
+    print(X_train_hog.shape)
+    np.save('data/X_train_hog', X_train_hog)
+
+    print("Extract HOG features of test samples...")
+    for i in range(X_test.shape[0]):
+        if (i+1) % 1000 == 0:
+            print("{}/{}".format(i+1, X_test.shape[0]))
+        feature, hog_img = hog(X_test[i], 9, (16, 16), (2, 2), visualise=True, feature_vector=True)
+        X_test_hog.append(feature)
+    X_test_hog = np.array(X_test_hog)
+    print("Finish extracting test samples!")
+    print(X_test_hog.shape)
+    np.save('data/X_test_hog', X_test_hog)
+
+    # i = 1
+    # feature, hog_img = hog(X_train[i], 9, (16, 16), (2, 2), visualise=True, feature_vector=True)
+    # hog_img = hog_img.reshape(96,96,1)
+    # hog_img = cv2.cvtColor(hog_img, cv2.COLOR_GRAY2BGR)
+    # print(hog_img)
+    # print(X_train[i].shape, hog_img.shape)
+
+    # img = np.concatenate((X_train[i], hog_img))
+    # cv2.imshow('hog', img)
+    # cv2.waitKey(0)
